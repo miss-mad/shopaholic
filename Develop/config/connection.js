211969199e3030e2d4386fc1898cf57d22984826
miss-mad <mmlh.harris@gmail.com>
2022-10-01 15:46:05 -0400

    connected to the database using Sequelize

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
+// dotenv allows sensitive info like passwords to be kept safe
+require("dotenv").config();
+// Import and require the npm package Sequelize
+const Sequelize = require("sequelize");
 
-const Sequelize = require('sequelize');
-
+// Create a sequelize connection object with dotenv variables
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: "localhost",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
     });
 
+// Export sequelize connection to be used elsewhere
 module.exports = sequelize;
--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const express = require("express");
+const routes = require("./routes");
+// Import sequelize connection
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Sync sequelize models to the database, then turn on the server
+// The force: true object serves to drop tables if any exist, then recreates them
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
