118edb57286d0b62bf6d34540b4c14b55dd65232
miss-mad <mmlh.harris@gmail.com>
2022-10-01 18:49:42 -0400

    associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,27 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// Product belongs to Category, as a category can have multiple products but a product can only belong to one category.
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+// Category has many Product models.
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+// Product belongs to many Tag models. Using the ProductTag through model, allow products to have multiple tags and tags to have many products.
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+// Tag belongs to many Product models.
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
