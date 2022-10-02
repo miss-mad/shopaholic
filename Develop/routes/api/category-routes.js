d8fc488fd5b1678b891f3cf550d8e3c70d99ba85
miss-mad <mmlh.harris@gmail.com>
2022-10-01 22:43:59 -0400

    category routes created

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,15 +2,30 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+// http://localhost:3001/api/categories = route
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [{ model: Product }],
+  }).then((allCategoryData) => {
+    res.status(200).json(allCategoryData);
+  });
 });
 
+// get one category
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [{ model: Product }],
+  }).then((oneCategoryData) => {
+    res.json(oneCategoryData);
+  });
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +38,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
